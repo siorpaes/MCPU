c69ac84a41724b78e4d297976f64c3ee695a1ee9
David Siorpaes <david.siorpaes@st.com>
2018-02-12 16:25:23 +0100

    Changed Goboard application to blinky

--- projects/Goboard/mcpu_toplevel.vhd
+++ projects/Goboard/mcpu_toplevel.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mcpu_toplevel is
-generic (CLK_DIVISOR :  POSITIVE := 1000000);
+generic (CLK_DIVISOR :  POSITIVE := 10000);
 port(
   clk     : in std_logic;
   reset   : in std_logic;
